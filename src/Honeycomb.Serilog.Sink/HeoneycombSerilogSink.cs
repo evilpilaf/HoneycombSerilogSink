f37fa212f6a0f56058b43df5cc57d40ce64b784a
mauricio-ddelc <mauriciod73@gmail.com>
2019-10-12 13:12:46 +0200

    Initial implementation

---
+++ src/Honeycomb.Serilog.Sink/HeoneycombSerilogSink.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+
+using Serilog.Core;
+using Serilog.Events;
+using Serilog.Formatting;
+using Serilog.Formatting.Compact;
+
+namespace Honeycomb.Serilog.Sink
+{
+    internal class HeoneycombSerilogSink : ILogEventSink
+    {
+        private static readonly Uri _honeycombApiUrl = new Uri("https://api.honeycomb.io/1/events/");
+        private readonly ITextFormatter _formatter;
+        private readonly string _teamId;
+        private readonly string _apiKey;
+
+        private readonly Lazy<HttpClient> _clientBuilder = new Lazy<HttpClient>(BuildHttpClient);
+        protected virtual HttpClient Client => _clientBuilder.Value;
+
+        public HeoneycombSerilogSink(string teamId)
+        {
+            _teamId = teamId;
+            _formatter = new CompactJsonFormatter();
+        }
+
+        public void Emit(LogEvent logEvent)
+        {
+            using (var buffer = new StringWriter(new StringBuilder()))
+            {
+                _formatter.Format(logEvent, buffer);
+                var formattedLogEventText = buffer.ToString();
+                var message = new HttpRequestMessage(HttpMethod.Post, $"/{_teamId}");
+                message.Content = new StringContent(formattedLogEventText);
+                message.Headers.Add("'X-Honeycomb-Team", _apiKey);
+                Client.SendAsync(message);
+            }
+        }
+
+        private static HttpClient BuildHttpClient()
+        {
+            var client = new HttpClient(new HttpClientHandler
+            {
+                AutomaticDecompression = DecompressionMethods.GZip
+            });
+            client.BaseAddress = _honeycombApiUrl;
+            return client;
+        }
+    }
+}
