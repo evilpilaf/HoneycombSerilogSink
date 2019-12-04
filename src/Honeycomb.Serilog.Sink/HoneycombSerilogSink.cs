e6b8d3fdfdec7d9f9c58be810946c9785550bf4c
mauricio-ddelc <mauriciod73@gmail.com>
2019-12-04 15:26:34 +0100

    Replace log event formatting to do it by hand to flatten the properties

--- src/Honeycomb.Serilog.Sink/HoneycombSerilogSink.cs
+++ src/Honeycomb.Serilog.Sink/HoneycombSerilogSink.cs
@@ -1,20 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net.Http;
 using System.Text;
 
 using Serilog.Core;
 using Serilog.Events;
-using Serilog.Formatting;
-using Serilog.Formatting.Compact;
-using Serilog.Formatting.Json;
 
 namespace Honeycomb.Serilog.Sink
 {
     internal class HoneycombSerilogSink : ILogEventSink
     {
         private static readonly Uri _honeycombApiUrl = new Uri("https://api.honeycomb.io/1/events/");
-        private readonly ITextFormatter _formatter;
+
         private readonly string _teamId;
         private readonly string _apiKey;
 
@@ -25,22 +24,37 @@ namespace Honeycomb.Serilog.Sink
         {
             _teamId = string.IsNullOrWhiteSpace(teamId) ? throw new ArgumentNullException(nameof(teamId)) : teamId;
             _apiKey = string.IsNullOrWhiteSpace(apiKey) ? throw new ArgumentNullException(nameof(apiKey)) : apiKey;
-            _formatter = new JsonFormatter();
         }
 
         public void Emit(LogEvent logEvent)
         {
             using (var buffer = new StringWriter(new StringBuilder()))
             {
-                _formatter.Format(logEvent, buffer);
-                var formattedLogEventText = buffer.ToString();
+                var evnt = BuildLogEvent(logEvent);
                 var message = new HttpRequestMessage(HttpMethod.Post, $"/{_teamId}")
                 {
-                    Content = new StringContent(formattedLogEventText)
+                    Content = new StringContent(evnt.ToString())
                 };
                 message.Headers.Add("X-Honeycomb-Team", _apiKey);
                 Client.SendAsync(message).ConfigureAwait(false);
             }
+        }
+
+        private static StringBuilder BuildLogEvent(LogEvent logEvent)
+        {
+            var evnt = new StringBuilder();
+            evnt.Append("{");
+            evnt.Append($"\"timestamp\": \"{logEvent.Timestamp:O}\",");
+            evnt.Append($"\"level\": \"{logEvent.Level}\",");
+            evnt.Append($"\"messageTemplate\": \"{logEvent.MessageTemplate}\",");
+            var propertyList = new List<string>(logEvent.Properties.Count());
+            foreach (var prop in logEvent.Properties)
+            {
+                propertyList.Add($"\"{prop.Key}\": {prop.Value.ToString()}");
+            }
+            evnt.Append(string.Join(",", propertyList));
+            evnt.Append("}");
+            return evnt;
         }
 
         private static HttpClient BuildHttpClient()
